35680225d27da55a65fe8b6d945e0719dd368895
DANYLO <agafonec1@gmail.com>
2024-02-27 17:28:36 +0200

    Remove old plugin reindex

--- templates/old_plugin_reindex.php
+++
@@ -1,4 +0,0 @@
-<p>If you were using plugin lower than version 3.0, you have to reindex your orders. this may take a while.</p>
-<form class="js-reindex-orders">
-    <button class="button" type="submit">Reindex Orders</button>
-</form>
